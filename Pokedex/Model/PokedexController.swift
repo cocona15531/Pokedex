894684a40e3d8de13fed7c3a8b818429354bd250
issei.ueda <ron.ron633662@gmail.com>
2024-04-30 23:39:56 +0900

    first commit

---
+++ Pokedex/Model/PokedexController.swift
@@ -0,0 +1,19 @@
+//
+//  PokedexController.swift
+//  Pokedex
+//
+//  Created by Issei Ueda on 2024/04/30.
+//
+
+import UIKit
+
+class PokedexController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .blue
+        print("PokedexController")
+    }
+    
+}
+
